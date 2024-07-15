caa0c2f0202336e4f18b9958e365cef6ad441bdd
stoganych <stoganych@gmail.com>
2024-07-15 20:27:10 +0300

    init commit

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,42 +1,71 @@
+const { units, conversionRate, unitMapping } = require("../utils/constants");
+
 function ConvertHandler() {
   
   this.getNum = function(input) {
-    let result;
-    
-    return result;
+    const englishAlphabet = /[a-zA-Z]/;
+    const idx = input.split("").findIndex((char) => englishAlphabet.test(char));
+    if (idx === 0) {
+      return 1;
+    }
+
+    let quantityStr;
+    if (idx < 0) {
+      quantityStr = input.slice(0);
+    } else {
+      quantityStr = input.slice(0, idx);
+    }
+
+    const quantityArr = quantityStr.split("/");
+
+    if (quantityArr.length === 1) {
+      const quantity = quantityArr[0];
+      if (quantity === "") return "invalid number";
+      return isNaN(+quantity) ? "invalid number" : +quantity;
+    }
+    if (quantityArr.length === 2) {
+      if (quantityArr.some((num) => num === "")) {
+        return "invalid number";
+      }
+      const numerator = +quantityArr[0];
+      const denominator = +quantityArr[1];
+      return isNaN(numerator) || isNaN(denominator)
+        ? "invalid number"
+        : numerator / denominator;
+    }
+
+    return "invalid number";
   };
   
   this.getUnit = function(input) {
-    let result;
-    
-    return result;
+    const englishAlphabet = /[a-zA-Z]/;
+    const idx = input.split("").findIndex((char) => englishAlphabet.test(char));
+    if (idx < 0) {
+      return "invalid unit";
+    }
+    const unit = input.slice(idx);
+    return this.spellOutUnit(unit);
   };
   
   this.getReturnUnit = function(initUnit) {
-    let result;
-    
-    return result;
+    return units[initUnit];
   };
 
   this.spellOutUnit = function(unit) {
-    let result;
-    
-    return result;
+    const targetUnit = unit.toLowerCase();
+    if (targetUnit === "l") return "L";
+    if (units.hasOwnProperty(targetUnit)) {
+      return targetUnit;;
+    }
+    return "invalid unit";
   };
   
   this.convert = function(initNum, initUnit) {
-    const galToL = 3.78541;
-    const lbsToKg = 0.453592;
-    const miToKm = 1.60934;
-    let result;
-    
-    return result;
+    return Math.round(conversionRate[initUnit] * initNum * 1e5) / 1e5;
   };
   
   this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    let result;
-    
-    return result;
+    return `${initNum} ${unitMapping[initUnit]} converts to ${returnNum} ${unitMapping[returnUnit]}`;
   };
   
 }
